d8660dcacea97ff1763f5d72ce784bbe40681206
suyatna <zuyatna@gmail.com>
2021-08-18 22:38:54 +0700

    35 - Write a Generic Method with Return Value

---
+++ WiredBrainCoffee-StorageApp/Entities/EntityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace WiredBrainCoffee_StorageApp.Entities
+{
+    public static class EntityExtensions
+    {
+        public static T? Copy<T>(this T itemCopy) where T : IEntity
+        {
+            var json = JsonSerializer.Serialize<T>(itemCopy);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
